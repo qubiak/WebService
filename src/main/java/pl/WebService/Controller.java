78866fd7074ce3ca86a5dafb4d7adff54cb9d5ee
qubiak <kubiak1986@gmail.com>
2020-09-21 19:15:32 +0200

    commit

---
+++ src/main/java/pl/WebService/Controller.java
@@ -0,0 +1,14 @@
+package pl.WebService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "Hello!!";
+    }
+}
